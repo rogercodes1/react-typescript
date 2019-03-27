0d73b2b2d194e4269cfc001bd5b7aff26255dc27
rogercodes1 <rogerperez.us@gmail.com>
2019-03-27 09:41:47 -0700

    added other ways to use typescript

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,16 @@ import './App.css';
 
 class App extends Component {
   render() {
-    let firstValue: string = 'Manny';
+    let firstValue: number = 100;
+    let secValue: boolean = true;
+    let thirdVal: number[] = [2,4,6];
+    let arrOpt: Array<string> = ["HEY", "Hello", "World"];
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-           The value {} is of {typeof firstValue} type!
+           The value {firstValue} is of {typeof firstValue} type!
           </p>
          
         </header>

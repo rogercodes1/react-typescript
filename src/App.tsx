196efe2fac401a3ab1a6893b14b666c9cfe9a25e
rogercodes1 <rogerperez.us@gmail.com>
2019-03-27 09:39:04 -0700

    First commit of the day

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,15 @@ import './App.css';
 
 class App extends Component {
   render() {
+    let firstValue: string = 'Manny';
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+           The value {} is of {typeof firstValue} type!
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+         
         </header>
       </div>
     );

fbadfc17fa451784c8b40bd8692048dac8ac5a02
rogercodes1 <rogerperez.us@gmail.com>
2019-03-27 09:45:16 -0700

    added tuple example

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,16 @@ class App extends Component {
     let secValue: boolean = true;
     let thirdVal: number[] = [2,4,6];
     let arrOpt: Array<string> = ["HEY", "Hello", "World"];
+    // tuple
+    let aTuple: [string, number] = ["Roger", 100]; 
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-           The value {firstValue} is of {typeof firstValue} type!
+           The value {firstValue} is of {typeof firstValue} type! <br/>
+           The value {aTuple[0]} is of {typeof aTuple[0]} type! <br/>
+           The value {aTuple[1]} is of {typeof aTuple[1]} type!
           </p>
          
         </header>
